f2e43a6f0a4844c57dcd72a7b7312faad77d0c8d
Rachel Wyatt <rw126@evansville.edu>
2019-01-12 14:19:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,13 +19,11 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		user = User.new(:username => params[:username], :password => params[:password])
-		redirect to "/failure"
-		binding.pry
-		#if 1==2
-			#redirect "/login"
-		#else
-			#redirect "/failure"
-		#end
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/login" do
